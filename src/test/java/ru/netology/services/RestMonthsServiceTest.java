70888ff8ab56f8f069283ccdd063b0d8cf3c85ba
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2023-11-15 20:01:38 +0300

    change from Test to ParameterizedTest

--- src/test/java/ru/netology/services/RestMonthsServiceTest.java
+++ src/test/java/ru/netology/services/RestMonthsServiceTest.java
@@ -1,26 +1,27 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestMonthsServiceTest {
-
-    @Test
-    void shouldCalculateThreeRestMonths() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/restmonths.csv")
+    void shouldCalculateRestMonths(int expected, int income, int expenses, int threshold) {
         RestMonthsService service = new RestMonthsService();
-        int expected = 3;
-        int actual = service.calcRestMonths(10_000, 3000, 20_000);
+        //int expected = 3;
+        int actual = service.calcRestMonths(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldCalculateTwoRestMonths() {
-        RestMonthsService service = new RestMonthsService();
-        int expected = 2;
-        int actual = service.calcRestMonths(100_000, 60_000, 150_000);
+    //@Test
+    //void shouldCalculateTwoRestMonths() {
+    //  RestMonthsService service = new RestMonthsService();
+    //int expected = 2;
+    //int actual = service.calcRestMonths(100_000, 60_000, 150_000);
 
-        Assertions.assertEquals(expected, actual);
-    }
+    //Assertions.assertEquals(expected, actual);
+    //}
 }
 
